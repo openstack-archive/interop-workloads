763602f505b39b6a57d974654cbb3371f5489bdd
yaohelan <yaohelan@huawei.com>
2017-03-01 04:36:15 -0500

    [WIP] NFV workload
    This workload accomplishes the following:
    1. Pull OPNFV Opera code
    2. Levarage Opera code to install OPEN-O, deploy Clearwater vIMS by
    OPEN-O
    3. Pull Clearwater live test code
    4. Run the test against Clearwater
    
    Change-Id: I994503fcb52711fb6cd3222bd151be854665b21c
    Signed-off-by: yaohelan <yaohelan@huawei.com>

---
+++ workloads/nfv/files/admin-openrc-v3-sample.sh
@@ -0,0 +1,8 @@
+export OS_PROJECT_DOMAIN_NAME=default
+export OS_USER_DOMAIN_NAME=default
+export OS_PROJECT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=password
+export OS_AUTH_URL=http://172.16.1.222:5000/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_REGION_NAME=RegionOne
